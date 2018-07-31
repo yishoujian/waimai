7ed4ccf583dee0e3c6a59a8a8ffb7f95915a3bf7
yishoujian <784259775@qq.com>
2018-07-31 21:32:09 +0800

    第八天 API接口  全部写完了

--- routes/api.php
+++ routes/api.php
@@ -16,19 +16,39 @@ use Illuminate\Http\Request;
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
 //    return $request->user();
 //});
-
+Route::domain('www.waimai.com')->namespace('Api')->group(function () {
     //首页接口
-    Route::any('shop/list', "Api\ShopController@list");
-    Route::any('shop/index', "Api\ShopController@index");
+    Route::any('shop/list', "ShopController@list");
+    Route::any('shop/index', "ShopController@index");
     //登录
-    Route::any('member/login', "Api\MemberController@login");
-    Route::any('member/reg', "Api\MemberController@reg");
-    Route::any('member/sms', "Api\MemberController@sms");
+    Route::any('member/login', "MemberController@login");
+    Route::any('member/reg', "MemberController@reg");
+    Route::any('member/sms', "MemberController@sms");
     //忘记密码
-Route::any('member/new', "Api\MemberController@new");
+    Route::any('member/new', "MemberController@new");
 //修改密码
-Route::any('member/edit', "Api\MemberController@edit");
+    Route::any('member/edit', "MemberController@edit");
+//添加收货地址
+    Route::any('address/add', "AddressController@add");
+    //收货地址列表
+    Route::any('address/list', "AddressController@list");
+    //修改收货地址回显
+    Route::any('address/edit', "AddressController@edit");
+    //修改收货地址
+    Route::any('address/update', "AddressController@update");
+    //购物车添加
+    Route::any('cart/add',"CartController@add");
+    //购物车列表
+    Route::any('cart/index',"CartController@index");
+    //添加订单表
+    Route::any('order/add',"OrderController@add");
+    //指定订单
+    Route::any('order/index',"OrderController@index");
+    //订单列表
+    Route::any('order/list',"OrderController@list");
+    //支付
+    Route::any('order/pay',"OrderController@pay");
 
-
+});
 
 
